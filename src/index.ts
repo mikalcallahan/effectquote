dbfe06e249f6f050d9c3bcab6b2735e3bb54bd5a
Mikal Callahan <mikal.callahan@icloud.com>
2024-05-02 19:41:26 -0400

    main - refactor: check keys as apart of json parse/decode

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Array, Console, Data, Effect, Option } from "effect";
+import { Array, Console, Data, Effect, Option, pipe } from "effect";
 import { catchAll, flatMap, map, tap } from "effect/Effect";
 import * as NodeFS from "node:fs";
 
@@ -15,7 +15,7 @@ const readFile = (filename: string) =>
             Effect.try({
               try: () => JSON.parse(data),
               catch: (error) => ({ code: "PARSE_ERROR", error }),
-            }),
+            }).pipe(Effect.flatMap((values) => decodeQuote(values))),
           );
         }
       });
@@ -23,6 +23,9 @@ const readFile = (filename: string) =>
   );
 
 const program = readFile(`${homedir}/.config/quotes/quotes.json`).pipe(
+  // const program = readFile(
+  //   `${homedir}/Developer/desktop/effectquote/src/quotes-error.json`,
+  // ).pipe(
   flatMap((value) => {
     return Effect.try({
       try: () => Data.array(value.quotes),
@@ -38,11 +41,11 @@ const program = readFile(`${homedir}/.config/quotes/quotes.json`).pipe(
   tap((quote) => Console.log(quote)),
   catchAll((error) => {
     if (error.code === "ENOENT") {
-      return Console.log("File not found");
+      return Console.log("Error: File not found");
     } else if (error.code === "PARSE_ERROR") {
-      return Console.log("JSON not formatted correctly");
+      return Console.log("Error: JSON not formatted correctly");
     } else {
-      return Console.log(error?.code);
+      return Console.log(`Error: ${error?.code}`);
     }
   }),
 );
@@ -58,4 +61,24 @@ const getRandomNumberInRange = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
 const formatQuote = (quote: Option.Option<Quote>) =>
-  Option.map(quote, (value) => `${value.text}\n- ${value.author ?? "butt"}`);
+  Option.map(quote, (value) => `${value.text}\n- ${value.author}`);
+
+const decodeQuote = (value: {
+  quotes: Array<Quote>;
+}): Effect.Effect<
+  { quotes: Array<Quote> },
+  { code: string; error: unknown }
+> => {
+  const hasQuotesKey = value.hasOwnProperty("quotes");
+  const hasSubKeys = value.quotes
+    .map(
+      (quote) => quote.hasOwnProperty("author") && quote.hasOwnProperty("text"),
+    )
+    .every((value) => value);
+  const hasKeys = hasQuotesKey && hasSubKeys;
+  return Effect.flatMap(Effect.succeed(hasKeys), () =>
+    hasKeys
+      ? Effect.succeed(value)
+      : Effect.fail({ code: "PARSE_ERROR", error: "" }),
+  );
+};
